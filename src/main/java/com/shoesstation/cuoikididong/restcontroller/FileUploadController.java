002b9cfdfba89313802bab9a23b22629d43a86cb
Duc Nguyen <ducnguyen201231@gmail.com>
2022-05-15 12:15:32 +0700

    make better

--- src/main/java/com/shoesstation/cuoikididong/restcontroller/FileUploadController.java
+++ src/main/java/com/shoesstation/cuoikididong/restcontroller/FileUploadController.java
@@ -1,35 +1,43 @@
 package com.shoesstation.cuoikididong.restcontroller;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.shoesstation.cuoikididong.exception.RestErrorResponse;
+import com.shoesstation.cuoikididong.service.CloudDinaryService;
 import com.shoesstation.cuoikididong.service.IStorageService;
 
 
 @Controller
-@RequestMapping(path ="/fileUpload" )
 public class FileUploadController {
 	@Autowired
 	private IStorageService storageService;
+	@Autowired
+	private CloudDinaryService cloudDinaryService;
 	
-	@PostMapping("")
+	@PostMapping("/fileUpload")
 	public ResponseEntity<RestErrorResponse> uploadFile(@RequestParam("file") MultipartFile file){
 		try {
-			
 			String generatedFileName=storageService.StoreFile(file);
 			return ResponseEntity.status(HttpStatus.OK).body(new RestErrorResponse(generatedFileName, HttpStatus.OK,LocalDateTime.now()));
 			
 		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(new RestErrorResponse(e.getMessage(), HttpStatus.OK,LocalDateTime.now()));
+			return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(new RestErrorResponse(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR,LocalDateTime.now()));
 		}
 	}
+	@PostMapping("/cloudDinary/fileUpload")
+	public ResponseEntity<RestErrorResponse> upLoad(@RequestParam("file") MultipartFile multipartFile) throws IOException{
+		Map<?, ?> resultMap=cloudDinaryService.upload(multipartFile);
+		return ResponseEntity.status(HttpStatus.OK).body(new RestErrorResponse(resultMap.get("url").toString(), HttpStatus.OK,LocalDateTime.now()));
+	}
+	
 }
