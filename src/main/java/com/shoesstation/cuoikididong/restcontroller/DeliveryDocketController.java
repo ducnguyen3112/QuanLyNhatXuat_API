ac951ac4bca204b534b6cda7824f155ca2e66861
Duc Nguyen <ducnguyen201231@gmail.com>
2022-06-07 17:11:36 +0700

    make better

--- src/main/java/com/shoesstation/cuoikididong/restcontroller/DeliveryDocketController.java
+++ src/main/java/com/shoesstation/cuoikididong/restcontroller/DeliveryDocketController.java
@@ -1,17 +1,17 @@
 package com.shoesstation.cuoikididong.restcontroller;
 
-import java.time.LocalDateTime;
+import java.lang.reflect.Field;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.util.ReflectionUtils;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -22,7 +22,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.shoesstation.cuoikididong.entity.DeliveryDocket;
 import com.shoesstation.cuoikididong.entity.DeliveryDocketDetail;
 import com.shoesstation.cuoikididong.exception.ResourceNotFoundException;
-import com.shoesstation.cuoikididong.exception.RestResponse;
 import com.shoesstation.cuoikididong.repository.DeliveryDocketRepository;
 
 
@@ -71,16 +70,35 @@ public class DeliveryDocketController {
 	
 	
 	
-	@DeleteMapping("/{id}")
-	public  ResponseEntity<RestResponse> deletedeliveryDocket(@PathVariable int id) {
+//	@DeleteMapping("/{id}")
+//	public  ResponseEntity<RestResponse> deletedeliveryDocket(@PathVariable int id) {
+//		Optional<DeliveryDocket> deliveryDocketOptional=deliveryDocketRepository.findById(id);
+//		if (!deliveryDocketOptional.isPresent()) {
+//			throw new ResourceNotFoundException("Không tìm thấy phiếu xuất có id: " +id);
+//		}
+//		deliveryDocketRepository.deleteById(id);
+//		return ResponseEntity.ok(new RestResponse("Phiếu xuất có id: "+id+" đã được xóa!", HttpStatus.OK,LocalDateTime.now()));
+//		
+//	}
+	@PatchMapping("/{id}")
+	public DeliveryDocket updateDeliveryDocketOnField(@RequestBody Map<Object, Object> fields, @PathVariable int id) {
 		Optional<DeliveryDocket> deliveryDocketOptional=deliveryDocketRepository.findById(id);
+		
 		if (!deliveryDocketOptional.isPresent()) {
 			throw new ResourceNotFoundException("Không tìm thấy phiếu xuất có id: " +id);
+		}else {
+			 fields.forEach((k, v) -> {
+		       // use reflection to get field k on manager and set it to value v
+		        Field field = ReflectionUtils.findRequiredField(DeliveryDocket.class, (String) k);
+		        field.setAccessible(true);
+		        ReflectionUtils.setField(field, deliveryDocketOptional.get(), v);
+		    });
+			
 		}
-		deliveryDocketRepository.deleteById(id);
-		return ResponseEntity.ok(new RestResponse("Phiếu xuất có id: "+id+" đã được xóa!", HttpStatus.OK,LocalDateTime.now()));
-		
+		return deliveryDocketRepository.save(deliveryDocketOptional.get());
+		 
 	}
+	
 	@GetMapping("/{id}/deliveryDocketDetails")
 	public List<DeliveryDocketDetail> getDeliveryDocketDetailById(@PathVariable int id) {
 	
